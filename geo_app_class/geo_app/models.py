e9a8f86d8e7722f79ef1b9048119768476f236d0
MrHarvvey <oskarflorczak@gmail.com>
2021-06-17 20:27:42 +0200

    working att data model 3

--- geo_app_class/geo_app/models.py
+++ geo_app_class/geo_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 import csv
+import yaml
+import re
 
 class StreetRecord:
     def __init__(self, data):
@@ -52,5 +54,40 @@ class CityFile(CSVFile):
             city = item[1]
             if city.city_name() == city_named:
                 return city.record_id()
+
+
+class UploadYaml:
+    _file = None
+    _what_download = None
+    def load_file(self):
+        with open(self._file, 'r') as yaml_file:
+            try:
+                yaml_file = yaml.safe_load(yaml_file)
+                self._data = yaml_file.get(self._what_download)
+            except yaml.YAMLError as exc:
+                return exc
+
+
+class Validation(UploadYaml):
+    _file = 'Validation_Errors.yaml'
+    _what_download = 'errors'
+    def search_error(self, error_code):
+        error = self._data.get(error_code).get('PL')
+        return error
+    def city_check(self, city_name):
+        if re.match("^[a-zA-Z]+$", city_name):
+            return city_name
+        else:
+            return self.search_error("ERROR-BAD-CITY")
+
+
+
+pliki = Validation()
+
+pliki.load_file()
+
+print(pliki.city_check("Skierniewice"))
+
+
 
 
