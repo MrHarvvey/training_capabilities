2c622393322ef032f0afe990a28fb8f18e345711
MrHarvvey <oskarflorczak@gmail.com>
2021-06-22 20:23:08 +0200

    validation 3

--- geo_app_class/geo_app/libs/validators.py
+++ geo_app_class/geo_app/libs/validators.py
@@ -1,7 +1,4 @@
 import yaml
-from ..models import CityFile
-
-
 
 class UploadErrors(Exception):
     _file = 'Validation_Errors.yaml'
@@ -10,25 +7,41 @@ class UploadErrors(Exception):
     _error_code = 'ERROR-BAD-REQ'
     error_desc = str
     _language = 'PL'
+    required = bool
 
-    def __init__(self):
+    def __init__(self, **params):
         with open(self._file, 'r') as yaml_file:
             yaml_file = yaml.safe_load(yaml_file)
             self._errors = yaml_file.get(self._category)
             UploadErrors.error_desc = self._errors.get(self._error_code).get(self._language)
+        for k, v in params.items():
+            if not k.startswith("_"):
+                if hasattr(self, k):
+                    self.__setattr__(k, v)
 
 #jak dodac tutaj
 class Validator(UploadErrors):
-    _error_code = 'ERROR-BAD-REQ'
+    _error_code = 'VALUE-IS-REQUIRED'
     _category = 'errors'
-    def __call__(self, *args, **kwargs):
-        return UploadErrors.error_desc
+    default_value = ""
+    def _is_value(self, value):
+        return bool(value)
+
+    def __call__(self, value):
+        if not self._is_value(value):
+            if self.required:
+                raise Validator
+            else:
+                return self.default_value
+        return value
+
 
 
 class ValCityLen(Validator):
     _error_code = "ERROR-STREET-DOES-NOT-FOUND"
     max_len = 45
     min_len = 4
+
     def __call__(self, value):
         if len(value) > ValCityLen.max_len:
             raise ValCityLen
@@ -36,24 +49,31 @@ class ValCityLen(Validator):
             raise ValCityLen
         return value
 
-
-
 class ValStreetLen(Validator):
-    _error_code = "ERROR-STREET-DOES-NOT-FOUND"
-    def __call__(self, value):
-        if len(value) < 20:
-            raise ValStreetLen
-        return value
+    max_len = 5
+    min_len = 4
+    _error_code = "ERROR-STREETLEN"
 
-class IsCity(Validator):
-    _error_code = "ERROR-NOT-IN-DATABASE"
-    def __call__(self, city):
-        object_city = CityFile()
-        object_city.load_file()
-        if object_city.search_city_id(city):
-            return city
+    def __call__(self, value):
+        var = super(ValStreetLen, self).__call__(value)
+        if len(value) > ValStreetLen.max_len:
+            raise ValStreetLen
+        elif len(value) < ValStreetLen.min_len:
+            raise ValStreetLen
         else:
-            raise IsCity
+            return value
+
+# class IsCity(Validator):
+#     _error_code = "ERROR-NOT-IN-DATABASE"
+#     def __call__(self, city):
+#         object_city = CityFile()
+#         object_city.load_file()
+#         if object_city.search_city_id(city):
+#             return city
+#         else:
+#             raise IsCity
+
+
 
 
 
--- geo_app_class/geo_app/views.py
+++ geo_app_class/geo_app/views.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from .models import CityFile, StreetFile
-from .libs.validators import IsCity
+from .libs.validators import ValCityLen, UploadErrors
 
 object_city = CityFile()
 object_city.load_file()
@@ -10,32 +10,54 @@ object_city.load_file()
 object_street = StreetFile()
 object_street.load_file()
 
-# @api_view(['POST'])
-# def street_search2(request):
-
+def validation_handling(fun):
+    def new_fun(*args, **kwargs):
+        try:
+            return fun(*args, **kwargs)
+        except UploadErrors as ex:
+            return {
+                "error": {
+                    "code": ex.error_desc
+                }
+            }
+    return new_fun
 @api_view(['POST'])
+@validation_handling
 def street_search(request):
-    #curl -X POST -H "Content-Type: application/json" -d @towns.json 127.0.0.1:8000/street/
-    error_bad_req = 4
     if request.method == 'POST':
-        try:
-            searched_city = request.data['city']
-            city_validator = IsCity()
-            valued_city = city_validator(searched_city)
-        except IsCity as ex:
-            error_bad_req = {'error': ex.error_desc}
-            return Response(error_bad_req, status=status.HTTP_200_OK)
-        try:
-            searched_city = object_city.search_city_id(searched_city)
-            if searched_city == None:
-                return Response(error_bad_req, status=status.HTTP_200_OK)
-            elif not object_street.search_street(searched_city):
-                error_req_streets = {'error': "City does not have any streets"}
-                return Response(error_req_streets, status=status.HTTP_200_OK)
-            else:
-                dict_1 = {'result': object_street.search_street(searched_city)}
-            return Response(dict_1, status=status.HTTP_200_OK)
-        except Exception as error:
-            print(error)
-            error_bad_req = {'error': "Something went wrong "}
-            return Response(error_bad_req, status=status.HTTP_400_BAD_REQUEST)
+        searched_city = request.data['city']
+        city_validator = ValCityLen()
+        valued_city = city_validator(searched_city)
+        city = object_city.search_city_id(valued_city)
+        streets = object_street.search_street(city)
+        data_response = {
+            "streets": streets
+        }
+    return Response(data_response, status=status.HTTP_200_OK)
+
+# @api_view(['POST'])
+# def street_search(request):
+#     #curl -X POST -H "Content-Type: application/json" -d @towns.json 127.0.0.1:8000/street/
+#     error_bad_req = 4
+#     if request.method == 'POST':
+#         try:
+#             searched_city = request.data['city']
+#             city_validator = IsCity()
+#             valued_city = city_validator(searched_city)
+#         except IsCity as ex:
+#             error_bad_req = {'error': ex.error_desc}
+#             return Response(error_bad_req, status=status.HTTP_200_OK)
+#         try:
+#             searched_city = object_city.search_city_id(searched_city)
+#             if searched_city == None:
+#                 return Response(error_bad_req, status=status.HTTP_200_OK)
+#             elif not object_street.search_street(searched_city):
+#                 error_req_streets = {'error': "City does not have any streets"}
+#                 return Response(error_req_streets, status=status.HTTP_200_OK)
+#             else:
+#                 dict_1 = {'result': object_street.search_street(searched_city)}
+#             return Response(dict_1, status=status.HTTP_200_OK)
+#         except Exception as error:
+#             print(error)
+#             error_bad_req = {'error': "Something went wrong "}
+#             return Response(error_bad_req, status=status.HTTP_400_BAD_REQUEST)
