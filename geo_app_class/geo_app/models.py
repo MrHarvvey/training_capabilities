a3821260710c7abb4f3c37f8040d2d5a3c7f4f44
MrHarvvey <oskarflorczak@gmail.com>
2021-06-27 23:45:24 +0200

    working at documentation

--- geo_app_class/geo_app/models.py
+++ geo_app_class/geo_app/models.py
@@ -1,27 +1,39 @@
-from django.db import models
 import csv
 
+
 class StreetRecord:
+
+    """Single street object parameters """
     def __init__(self, data):
         self._data = data
+
     def record_id(self):
         return self._data[4] + self._data[5]
+
     def city_id(self):
         return self._data[4]
+
     def street_name(self):
         return self._data[7]
+
     def to_json(self):
         return {
             'street': self.street_name()
         }
 
+
 class CityRecord:
+
+    """Single City object parameters """
     def __init__(self, data):
         self._data = data
+
     def record_id(self):
         return self._data[7]
+
     def city_name(self):
         return self._data[6]
+
     def to_json(self):
         return {
             'city': self.city_name()
@@ -29,6 +41,8 @@ class CityRecord:
 
 
 class CSVFile:
+
+    """Importing csv file into dictionary _data object, where primary key is record_id"""
     _data = None
     _file_name = None
     _class_object: type
@@ -42,8 +56,12 @@ class CSVFile:
 
 
 class StreetFile(CSVFile):
+
+    """searching and operating on Street object file"""
+
     _file_name = "ULIC.csv"
     _class_object = StreetRecord
+
     def search_street(self, city_id):
         street_list = []
         for item in self._data.items():
@@ -52,9 +70,14 @@ class StreetFile(CSVFile):
                 street_list.append(object.street_name())
         return street_list
 
+
 class CityFile(CSVFile):
+
+    """searching and operating on City object file"""
+
     _file_name = "SIMC.csv"
     _class_object = CityRecord
+
     def search_city_id(self, city_named):
         for item in self._data.items():
             city = item[1]
