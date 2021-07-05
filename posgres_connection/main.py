9cfd445ee0f9c2d3af732892ac3b37273f0c1f8f
MrHarvvey <oskarflorczak@gmail.com>
2021-07-05 23:26:10 +0200

    postgress working att mok

--- posgres_connection/main.py
+++ posgres_connection/main.py
@@ -11,8 +11,12 @@ class DatabaseMok:
                      'erntykat34@hotmail.com']
     # connect to the db
     _cursor = None
+    fetched_object = None
+
+    """Database connection """
 
     def __init__(self):
+
         self._con = psycopg2.connect(
             host="172.17.0.1",
             database="postgres",
@@ -22,18 +26,13 @@ class DatabaseMok:
         # cursor
         self._cursor = self._con.cursor()
 
+    """inserting Data into table mok_data"""
+
     def insert_data(self, age, email, name, surname, account_number):
 
         self._cursor.execute("insert into mok_data (age, email, name, surname, account_number) values (%s, %s, %s, %s, %s)", (age, email, name, surname, account_number))
         self._con.commit()
-        #self._con.close()
 
-    def __del__(self):
-        self._cursor = self._con.cursor()
-
-    def search_data(self):
-        self._cursor.execute("select * from mok_data ")
-        self._cursor.fetchall()
 
     def mok_times_data(self, value):
         for i in range(value):
@@ -44,35 +43,17 @@ class DatabaseMok:
 
             account_number = ''.join(["{}".format(randint(0, 9)) for _ in range(0, 24)])
             self.insert_data(age, email, name, surname, account_number)
+    def search_data(self):
+        self._cursor.execute("select age, email, name, surname, account_number from mok_data where id = 5678")
+        self.fetched_object = self._cursor.fetchall()
+
+
 
 data = DatabaseMok()
 
-data.mok_times_data(120000)
+#data.mok_times_data(120000)
+data.search_data()
+print(data.fetched_object)
 
 
 
-
-
-
-
-#mok_times_data(100000)
-
-
-
-# cur.execute("insert into rachunki_bankowe (nr_rachunku, wlasciciel_rachunku, saldo, waluta) values (%s, %s, %s, %s)", ('2323232323232623232323234', "Hussein", 323, 2))
-#
-# #execute query
-# cur.execute("select * from rachunki_bankowe")
-#
-# rows = cur.fetchall()
-#
-# for r in rows:
-#     print(f"id {r[0]} name {r[1]}")
-#
-# #commit the transcation
-# con.commit()
-#
-# #close the cursor
-# cur.close()
-
-#close the connection
